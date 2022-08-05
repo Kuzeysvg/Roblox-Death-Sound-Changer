e995f2c47b09c37975ee40f364c28b53d18de690
Kuzey <73121297+Kuzeysvg@users.noreply.github.com>
2022-08-05 12:15:39 +0300

    Add files via upload

---
+++ Change Roblox Death/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Change_Roblox_Death
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.message = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.Version = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.Version);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(167, 355);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(15, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(122, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "No current update log";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 17.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.Gray;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Update Logs";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(171, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(154, 25);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "How can i use it?";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(173, 29);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(460, 76);
+            this.label4.TabIndex = 2;
+            this.label4.Text = resources.GetString("label4.Text");
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 12.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(506, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(117, 46);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Change it!";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // message
+            // 
+            this.message.AutoSize = true;
+            this.message.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.message.Location = new System.Drawing.Point(185, 269);
+            this.message.Name = "message";
+            this.message.Size = new System.Drawing.Size(105, 19);
+            this.message.TabIndex = 6;
+            this.message.Text = "No File Selected";
+            // 
+            // panel2
+            // 
+            this.panel2.AllowDrop = true;
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(55)))), ((int)(((byte)(55)))), ((int)(((byte)(55)))));
+            this.panel2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(177, 108);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(446, 149);
+            this.panel2.TabIndex = 7;
+            this.panel2.DragDrop += new System.Windows.Forms.DragEventHandler(this.panel2_DragDrop_1);
+            this.panel2.DragEnter += new System.Windows.Forms.DragEventHandler(this.panel2_DragEnter_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(133, 55);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(157, 30);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Drag and Drop";
+            // 
+            // Version
+            // 
+            this.Version.AutoSize = true;
+            this.Version.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Version.Location = new System.Drawing.Point(-1, 340);
+            this.Version.Name = "Version";
+            this.Version.Size = new System.Drawing.Size(63, 13);
+            this.Version.TabIndex = 8;
+            this.Version.Text = "Version 1.0";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel1.LinkColor = System.Drawing.Color.Blue;
+            this.linkLabel1.Location = new System.Drawing.Point(171, 337);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(154, 13);
+            this.linkLabel1.TabIndex = 8;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "https://github.com/Kuzeysvg";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(635, 355);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.message);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.Text = "Roblox Death Sound Changer";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label message;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label Version;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Change Roblox Death/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Change_Roblox_Death
+{
+    public partial class Form1 : Form
+    {
+        string[] fileDrop = {};
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        public void panel2_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        public void window_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        public void panel2_DragDrop(object sender, DragEventArgs e)
+        {
+            
+        }
+
+        public void panel2_DragDrop_1(object sender, DragEventArgs e)
+        {
+            string[] fileDrop = (string[])e.Data.GetData(DataFormats.FileDrop);
+            if (fileDrop[0].Contains(".ogg"))
+            {
+                try {
+                message.Text = fileDrop[0];
+                message.BackColor = Color.FromArgb(64, 64, 64);
+                message.ForeColor = Color.Black;
+                }
+
+                catch {
+                    message.Text = "An error ocurred";
+                    message.BackColor = Color.Red;
+                    message.ForeColor = Color.White;
+                }
+            }
+            else
+            {
+                message.Text = "Please check if the file is an ogg file or remove the dot on your folder.";
+                message.BackColor = Color.Red;
+                message.ForeColor = Color.White;
+            }
+        }
+
+        public void button1_Click(object sender, EventArgs e)
+        {
+            if (message.Text.Contains(".ogg"))
+            {
+                string oofFile = "/content/sounds/ouch.ogg";
+                string robloxPath = "C:/Users/" + Environment.UserName + "/AppData/Local/Roblox/Versions";
+                string[] versionList = Directory.GetDirectories(robloxPath);
+                for (int i = 0; i < versionList.Length; i++)
+                {
+                    if (versionList[i].Contains("version-"))
+                    {
+                        try 
+                        {
+                            string oofFileDir = Path.Combine(versionList[i] + oofFile);
+                            File.Delete(oofFileDir);
+                            File.Copy(message.Text, message.Text + "2");
+                            File.Move(message.Text, oofFileDir);
+                            File.Move(message.Text + "2", message.Text);
+                        }
+                        catch
+                        {
+                            message.Text = "An error ocurred while changing the sound";
+                            message.BackColor = Color.Red;
+                            message.ForeColor = Color.White;
+                        }
+                    }
+                    
+                }
+                MessageBox.Show("Successfully changed roblox death sound.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void panel2_DragEnter_1(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.All;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/Kuzeysvg");
+        }
+    }
+}
